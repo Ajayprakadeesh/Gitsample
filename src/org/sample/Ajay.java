d2ee74d868a435d8e31d38154df475bf68ccc289
manojkumarpc025 <manojkumarpc025@gmail.com>
2023-07-26 16:29:16 +0530

    first project

---
+++ src/org/sample/Ajay.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Ajay {
+	
+	
+	public static void main(String[] args) {
+		int a =25;
+		System.out.println("print a value :"+a);
+		
+		
+		 System.out.println("a work done");
+	}
+
+}
